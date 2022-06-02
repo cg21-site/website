21211e933dd00c2db65ea6097783eee1ded06852
cg21-site <106387879+cg21-site@users.noreply.github.com>
2022-06-02 13:27:21 +0530

    set collapseBooleanAttributes to true

--- .eleventy.js
+++ .eleventy.js
@@ -116,7 +116,8 @@ module.exports = function (eleventyConfig) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
+        collapseWhitespace: true,
+	collapseBooleanAttributes: true
       });
       return minified;
     }
